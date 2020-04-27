81c57d127df794ff3ff6d34a7183c708d8594b7f
Björn Carlsson <bjorncarlsson87@gmail.com>
2020-04-27 18:23:49 +0200

    creating login with data from users db

--- app.py
+++ app.py
@@ -1,26 +1,38 @@
-from flask import Flask, render_template, redirect, url_for, request, session, flash
-from functools import wraps
+from flask import Flask, render_template, redirect, url_for, request, flash
 from forms import LoginForm, RegisterForm
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_user, login_required, logout_user
+
+#from flask.ext.sqlalchemy import SQLAlchemy
+#from sqlalchemy import create_engine, text
+import psycopg2
 #from flask.ext.login import login_user, login_required, logout_user
 
 app = Flask(__name__)
-
+bcrypt = Bcrypt(app)
+#db = create_engine("postgresql://localhost/postgres")
+#db = create_engine("postgresql+psycopg2://localhost/postgres")
 #app.secret_key = "mySecretKey"
 
 # config
 import os
 app.config.from_object(os.environ['APP_SETTINGS'])
+login_manager = LoginManager()
+login_manager.init_app(app)
+db = SQLAlchemy(app)
+
+
+#print(db)
 #print(app.config)
-# login required decorator
-def login_required(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return f(*args, **kwargs)
-        else:
-            flash('You need to login first.')
-            return redirect(url_for('login'))
-    return wrap
+
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+	from models import User
+	return User.query.filter(User.id == int(user_id)).first()
+
 
 @app.route('/')
 @login_required
@@ -49,19 +61,28 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	from models import User
 	error = None
+	form = LoginForm()
 	if request.method == 'POST':
-		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-			error = 'Invalid credentials. Please try again!'
-		else:
-			session['logged_in'] = True
-			flash('You were just logged in')
-			return redirect(url_for('home'))
-	return render_template("login.html", error=error)
+		if form.validate_on_submit():
+			user = User.query.filter_by(name=request.form['username']).first()
+			if user is not None and bcrypt.check_password_hash(user.password, request.form['password']):
+			#user.password == request.form['password']:
+			#if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+				login_user(user)
+				#session['logged_in'] = True
+				flash('You were just logged in as: ' + str(user.name))
+				return redirect(url_for('home'))
+
+			else:
+				error = 'Invalid credentials. Please try again!'
+	return render_template("login.html", form=form, error=error)
 
 @app.route('/logout')
+@login_required
 def logout():
-	session.pop('logged_in', None)
+	logout_user()
 	flash('You were just logged out')
 	return redirect(url_for('welcome'))
 
--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 # default config
 class BaseConfig(object):
 	DEBUG = False
-	SECRET_KEY = 'mySecretKeyBase'
+	SECRET_KEY = '\xad\xea\xbec\xe8\xbeK$)\xb6\x06\xc8\xda\x00\xb5!\xa2\xf0u.\xda \x98\xe2'
+	SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/greenero_db'
 
 # dev config
 class DevelopmentConfig(BaseConfig):
 	DEBUG = True
-	SECRET_KEY = 'mySecretKeyDev'
+	#SECRET_KEY = 'mySecretKeyDev'
 
 
 # prod config
